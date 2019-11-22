a68b43bfd66996e959bcbb2b8cd94b31586f7e52
Tolsti <tolsti_web@mail.ru>
2019-11-22 10:41:08 +0300

    поисковая система по валюте и погоде в следующих версиях

--- app/get_weather.py
+++ app/get_weather.py
@@ -5,10 +5,16 @@ from pprint import pprint
 # appid = '94d8b74c4d0c302754e1af1f42419289'
 # http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
 
-def get_weather_city(city='Minsk'):
+def get_weather_city(city = 'Minsk'):
     app_id = "94d8b74c4d0c302754e1af1f42419289"
-    return requests.get(
+    data = dict()
+    weather_city = requests.get(
         'http://api.openweathermap.org/data/2.5/weather?appid={}&q={}&units=metric'.format(app_id, city)).json()
-
-print(get_weather_city()['main']['temp'])
-
+    data['city'] = weather_city['name']
+    data['temp'] = weather_city['main']['temp']
+    data['temp_max'] = weather_city['main']['temp_max']
+    data['temp_min'] = weather_city['main']['temp_min']
+    data['humidity'] = weather_city['main']['humidity']
+    data['pressure'] = weather_city['main']['pressure']
+    return data
+print(get_weather_city())
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 from app.get_films import data_films
 from app.get_courses import rates
 from app.get_weather import get_weather_city
-from pprint import pprint
+
 
 
 @app.route('/')
@@ -15,15 +15,15 @@ def index():
 
 @app.route('/courses')
 def courses():
-    return render_template('courses.html', data_now=datetime.datetime.now().date(), rates=rates)
+    return render_template('courses.html', data_now = datetime.datetime.now().date(), rates = rates)
 
 
 @app.route('/movies')
 def movies():
-    return render_template('movies.html', data_now=datetime.datetime.now().date(), data_films=data_films)
+    return render_template('movies.html', data_now = datetime.datetime.now().date(), data_films = data_films)
 
 
 @app.route('/weather')
 def weather():
-    data_weather=get_weather_city
-    return render_template('weather.html', data_now=datetime.datetime.now().date(), data_weather=data_weather)
+    return render_template('weather.html', data_now = datetime.datetime.now().date(), data_weather = get_weather_city())
+print(get_weather_city())
