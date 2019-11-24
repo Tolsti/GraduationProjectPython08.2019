f7b0e25d46b7a81492a55dedf4a9964ce6f7d5b8
Tolsti <tolsti_web@mail.ru>
2019-11-24 14:32:59 +0300

    поисковая система по валюте и погоде в следующих версиях

--- app/get_weather.py
+++ app/get_weather.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 # appid = '94d8b74c4d0c302754e1af1f42419289'
 # http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+# pro.openweathermap.org/data/2.5/forecast/hourly?id=524901
 
 def get_weather_city(city = 'Minsk'):
     app_id = "94d8b74c4d0c302754e1af1f42419289"
@@ -17,4 +18,10 @@ def get_weather_city(city = 'Minsk'):
     data['humidity'] = weather_city['main']['humidity']
     data['pressure'] = weather_city['main']['pressure']
     return data
-print(get_weather_city())
+
+
+def get_weather_hours(city = 'Minsk'):
+    app_id = "94d8b74c4d0c302754e1af1f42419289"
+    weather_city = requests.get(
+        'http://api.openweathermap.org/data/2.5/weather?appid={}&q={}&units=metric'.format(app_id, city)).json()
+    return weather_city
